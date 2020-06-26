1d497791b7b32c1011a7a308920a3a9c76ea473a
luisnovaish <luis.novais@engenharia.ufjf.br>
2020-06-26 15:58:13 -0300

    added email and password fields

--- Aula_05/s_i.js
+++ Aula_05/s_i.js
@@ -6,12 +6,12 @@
 /////////////////////////////////////////////////////////////////
 
 const onButtonClickIndex = () => {
-    obj = {
+    const obj = {
         email:     document.getElementById("emailInput").value,
         password:  document.getElementById("senhaInput").value,
     };
 
-    location.href = "./form.html"
+    location.href = "./main.html"
 }
 
 
